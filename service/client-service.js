569c6ce2a82147972e05d000b727b00545043aae
Ivan Pisquiy <pisquiy22@gmail.com>
2023-05-15 18:22:28 -0600

    Adding the function of eliminating and adding new clients

--- service/client-service.js
+++ service/client-service.js
@@ -3,6 +3,24 @@ const listaCliente = () => {
     );
 };
 
+const crearCliente = (nombre, email) => {
+    return fetch("http://localhost:3000/perfil", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({nombre, email, id: uuid.v4()})
+    });
+}
+
+const eliminarCliente = (id) => {
+    return fetch(`http://localhost:3000/perfil/${id}`, {
+        method: "DELETE",
+    })
+}
+
 export const clientServices = {
     listaCliente,
+    crearCliente,
+    eliminarCliente
 };
